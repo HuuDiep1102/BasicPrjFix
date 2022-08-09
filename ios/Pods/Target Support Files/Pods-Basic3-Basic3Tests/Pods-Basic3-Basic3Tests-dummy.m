d1b0f6ff8f979a06b2e4345a2a23095e086f5e04
HuuDiep1102 <56430293+HuuDiep1102@users.noreply.github.com>
2022-08-09 17:01:28 +0700

    doen

---
+++ ios/Pods/Target Support Files/Pods-Basic3-Basic3Tests/Pods-Basic3-Basic3Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Basic3_Basic3Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Basic3_Basic3Tests
+@end
---
+++ ios/Pods/Target Support Files/Pods-Basic3/Pods-Basic3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Basic3 : NSObject
+@end
+@implementation PodsDummy_Pods_Basic3
+@end
